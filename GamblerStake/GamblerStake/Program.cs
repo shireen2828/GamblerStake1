369513e63925719361341203f9db8a9ae4eb1517
unknown <shireenkk28@gmail.com>
2021-03-10 11:42:44 +0530

    [shireen]added GamblerStake

---
+++ GamblerStake/GamblerStake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblerStake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Stake_Everyday = 100;
+            int Stake_bet = 1;
+        }
+    }
+}
