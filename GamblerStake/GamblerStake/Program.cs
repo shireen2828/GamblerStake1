a49baf788a93548c334fd25326d8bb64ec4a3291
unknown <shireenkk28@gmail.com>
2021-03-10 12:35:33 +0530

    [shireen]added UC2

--- GamblerStake/GamblerStake/Program.cs
+++ GamblerStake/GamblerStake/Program.cs
@@ -8,6 +8,22 @@ namespace GamblerStake
         {
             int Stake_Everyday = 100;
             int Stake_bet = 1;
+            int Gambler = 0;
+            int win = 1;
+            int loose = 0;
+            Random random = new Random();
+            int GamblerCheck = random.Next(0, 2);
+            switch (GamblerCheck)
+            {
+                case win:
+                    Gambler = 1;
+                    break;
+                case loose:
+                    Gambler = 0;
+                    break;
+            }
+            Console.WriteLine("Gambler has: " + Gambler);
+            Console.ReadKey();
         }
     }
 }
